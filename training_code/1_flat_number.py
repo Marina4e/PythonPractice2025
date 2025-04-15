1f1153e66c6f961fd10bc9dfb5374a017ffa0a78
Marina Che <acc13072021@gmail.com>
2025-04-15 17:27:02 +0300

    Types

--- training_code/1_flat_number.py
+++ training_code/1_flat_number.py
@@ -4,3 +4,16 @@ entrance_number = (flat_number -1) //20 + 1
 floor_number = (flat_number - 1) % 20 // 4 + 1
 print("подъезд №" + str(entrance_number))
 print("квартира №" + str(floor_number))
+
+print("=" * 42)
+
+x = float(input('Input digit'))
+y = float(input('Input second digit'))
+r = x + y
+print('Result = ' + str(r))
+
+print("=" * 42)
+
+c = input("Entrance")
+print(type(c))
+
---
+++ training_code/2_types.py
@@ -0,0 +1,44 @@
+a = None
+print(type(a))
+b = 1
+print(type(b))
+c = 1.0
+print(type(c))
+d = 1 + 2j
+print(type(d))
+e = 'a'
+print(type(e))
+f = [1, 4, 'a']
+print(type(f))
+j = (1, 3, 'd')
+print(type(j))
+h = {'a': 1, 'd':3}
+print(type(h))
+i = True
+print(type(i))
+
+*o, x, n = [1, 2, 3, 5, 6, 8]
+print(o)
+print(x)
+print(n)
+
+print('='* 50)
+
+numb = 3
+numb = 4
+numb2 = 5
+res = numb + numb2
+print(res)
+
+numb1 = numb3 = 5
+print(numb1, numb3)
+print(numb1 - numb3)
+
+swap1 = 8
+swap2 = 9
+swap1, swap2 = swap2, swap1 + swap2
+print(swap1, swap2)
+
+
+swap2 -= numb1
+print(swap2)
